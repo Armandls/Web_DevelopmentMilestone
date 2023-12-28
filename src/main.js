16ef16823d66c1b3f051371f8fac5342ab707442
Carlos Romero <c.romero@students.salle.url.edu>
2023-12-28 21:00:15 +0100

    Importado el icono de rendirse (banderita)

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ import './style.css'
 import App from './App.vue'
 import router from "./router/index.js";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faUser, faShoppingCart, faList, faTrophy } from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faShoppingCart, faList, faTrophy, faFlag } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faHome, faUser, faShoppingCart, faList, faTrophy);
+library.add(faHome, faUser, faShoppingCart, faList, faTrophy, faFlag);
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
-
