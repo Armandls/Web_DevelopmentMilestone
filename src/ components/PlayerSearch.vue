b89276c411b3f433f635695476a053cd527f00d0
oriol <oriol.rebordosa@students.salle.url.edu>
2024-01-03 10:30:05 +0100

    Usabilitat

--- src/ components/PlayerSearch.vue
+++ src/ components/PlayerSearch.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex justify-center mt-4 bg-transparent">
-    <input v-model="searchText" @keyup.enter="performSearch" type="text" placeholder="Search Players..." class="p-3 w-full max-w-2xl bg-white border-2 border-gray-300 text-black placeholder-gray-500 rounded-l text-xl font-bold mt-10 ml-24">
+    <input v-model="searchText" @keyup.enter="emitSearch" type="text" placeholder="Search Players..." class="p-3 w-full max-w-2xl bg-white border-2 border-gray-300 text-black placeholder-gray-500 rounded-l text-xl font-bold mt-10 ml-24">
 
     <button @click="emitSearch" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r mt-10 mr-2">
       Search
